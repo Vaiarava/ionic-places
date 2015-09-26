cf13830a7ef563c5a3086607b7d70d7dbaab3e12
Vaiarava <vaiarava.wong@gmail.com>
2015-09-27 01:35:40 +0200

    select button in progress

--- www/src/places/media/media.controller.js
+++ www/src/places/media/media.controller.js
@@ -1,25 +1,34 @@
 angular.module('places')
 .controller('mediaCtrl', function($scope, $cordovaMedia, $ionicPlatform) {
   $ionicPlatform.ready(function() {
-$scope.music=function(){
-   window.plugins.iOSAudioPicker.getAudio(success,error,multiple,icloud);}
-   var multiple = 'false' // Will allow user to select multiple songs.
+    $scope.music=function(){
+      window.plugins.iOSAudioPicker.getAudio(success,error,'false','false');
+    };
+    //var multiple = 'false'; // Will allow user to select multiple songs.
+    //var icloud = 'false'; // Will only show songs available locally on device.
 
-   var icloud = 'false' // Will only show songs available locally on device.
+    $scope.playsong = function() {
+      //console.log('foo');
+      document.querySelector('#debug').innerHTML = 'texte';
+      debugDiv = document.querySelector('#debug');
+      debugDiv.innerHTML = 'DEBUG PLAY SONG<br/>';
+    }
   });
 
 
 function success(data)
 {
+  debugDiv = document.querySelector('#debug');
+  debugDiv.innerHTML = 'DEBUG<br/>' + JSON.stringify(data);
   console.log(JSON.stringify(data));
 }
 
 function error(e)
     {
+      debugDiv = document.querySelector('#debug');
+      debugDiv.innerHTML = 'ERROR<br/>';
       console.log(e);
     }
-
-
 
   /*
 	$scope.play = function() {
