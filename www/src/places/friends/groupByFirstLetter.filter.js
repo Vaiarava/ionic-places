8ab984d6332735450963c38b71cccc81dcaaef23
Vaiarava <vaiarava.wong@gmail.com>
2015-10-04 18:05:13 +0200

    delete button

--- www/src/places/friends/groupByFirstLetter.filter.js
+++
@@ -1,7 +0,0 @@
-angular.module('places').filter('groupByFirstLetter',function(){
-	return function(array) {
-		return _.groupBy(array, function(n) {
-			return n.name.substring(0,1);
-		});
-	};
-});
